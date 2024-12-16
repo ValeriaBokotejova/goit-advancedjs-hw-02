71095785b8eed74e4e6676baac42ba43faee849b
tabira <valeria.bokotejova@gmail.com>
2024-12-16 22:01:22 +0100

    add snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,49 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(event.currentTarget.elements.delay.value);
+  const state = event.currentTarget.elements.state.value;
+
+  console.log(`Delay: ${delay}`);
+  console.log(`State: ${state}`);
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      switch (state) {
+        case 'fulfilled':
+          resolve(delay);
+          break;
+        case 'rejected':
+          reject(delay);
+          break;
+        default:
+          reject('Invalid state');
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: '✅',
+        message: `Fulfilled promise in ${delay}ms`,
+        icon: false,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: '❌',
+        message: `Rejected promise in ${delay}ms`,
+        icon: false,
+        position: 'topRight',
+      });
+    });
+
+  event.currentTarget.reset();
+});
